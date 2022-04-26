e41be14e62bc42c42c587637edc8bb76a4ea935f
boris.atanassow@freenet.de <boris.atanassow@freenet.de>
2022-04-26 21:10:16 +0200

    fixed naming

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -5,8 +5,8 @@ import csv
 def load_sensor_data():
     sensor_data = []
     sensor_files = glob.glob(os.path.join(os.getcwd(), 'datasets', '*.csv'))
-    for room in sensor_files:
-        with open(room, encoding='utf-8') as data_file:
+    for sensor_file in sensor_files:
+        with open(sensor_file, encoding='utf-8') as data_file:
             data_reader = csv.DictReader(data_file, delimiter=',')
             for row in data_file:
                 sensor_data.append(row)
